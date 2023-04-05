d068657f7dd55f3eb9e1015ea6faa1d00c5cecca
Kevin Malik Fajar <kevinmalikfajar9@gmail.com>
2023-04-05 16:21:31 +0700

    Commit all file latihan react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Language from './Language';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: 'Ninja Ken', count : 0};
+  }
+
+  handleClick(name) {
+    this.setState({name: name});
+  }
+
+  plusValue() {
+    this.setState({count: this.state.count + 1});
+  }
+
+  minusValue() {
+    this.setState({count: this.state.count - 1});
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Halo, {this.state.name}!</h1>
+        <h2>Nilai saya adalah {this.state.count}</h2>
+
+        <button onClick={() => {this.handleClick('Guru Domba')}}>Guru Domba</button>
+        
+        {/* Ubah state untuk event onClick */}
+        <button onClick={() => {this.handleClick('Ninja Ken')}}>Ninja Ken</button>
+        
+        {/* Ubah state untuk event onClick */}
+        <button onClick={() => {this.plusValue()}}>Tambah Nilai</button>
+
+        {/* Ubah state untuk event onClick */}
+        <button onClick={() => {this.minusValue()}}>Kurangi Nilai</button>
+
+        <div className='language'>
+          <Language 
+            name='HTML & CSS'
+            image='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/html.svg'
+          />
+          <Language 
+            name='JavaScript'
+            image='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/es6.svg'
+          />
+          <Language 
+            name='React'
+            image='https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/react.svg'
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
